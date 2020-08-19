65ae63ad63ce4c80e0116eb0e5b27758b8a3908c
trunkslamchest <trunk9slamchest@gmail.com>
2020-08-19 14:03:00 -0400

    installed redux

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,12 @@ const firebase = require("firebase")
 const admin = require('firebase-admin')
 
 const url = {
-  // database: 'http://localhost:9000?ns=smartapp-b3d27',
-  // rootSecured: 'https://localhost:3000',
-  // rootUnsecured: 'http://localhost:3000'
-  database: 'https://smartapp-b3d27.firebaseio.com',
-  rootSecured: 'https://trunkslamchest.com',
-  rootUnsecured: 'http://trunkslamchest.com'
+  database: 'http://localhost:9000?ns=smartapp-b3d27',
+  rootSecured: 'https://localhost:3000',
+  rootUnsecured: 'http://localhost:3000'
+  // database: 'https://smartapp-b3d27.firebaseio.com',
+  // rootSecured: 'https://trunkslamchest.com',
+  // rootUnsecured: 'http://trunkslamchest.com'
 }
 
 var firebaseConfig = {
@@ -22,3 +22,16 @@ admin.initializeApp()
 
 // var db = init.database()
 // init.database()
+
+exports.test1 = functions
+  .region('us-east1')
+  .https.onRequest((req, res) => {
+
+    res.set('Access-Control-Allow-Methods', ['GET', 'OPTIONS'])
+
+    if(req.headers.origin === url.rootSecured || url.rootUnsecured ) { res.set('Access-Control-Allow-Origin', `${req.headers.origin}`) }
+
+    res.status(200).send('test1 successful')
+
+  return parsed
+});
