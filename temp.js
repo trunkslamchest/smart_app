da7164006744883717131cd0507a1d113f7e28ab
trunkslamchest <trunk9slamchest@gmail.com>
2020-08-19 14:54:23 -0400

    temporarily removed admin panel

---
+++ temp.js
@@ -0,0 +1,18 @@
+// import trafficFunctions from './utility/trafficFunctions'
+
+// onClickTrafficFunctions = (event, user) => {
+//   let elementInfo = {
+//     user_id: this.state.user.id || user,
+//     interaction: event.target.attributes.interaction.value,
+//     element: event.target.name
+//   }
+//   trafficFunctions('element', 'http://localhost:3001/traffics', elementInfo)
+// }
+
+// onPageLoadFunctions = ( page ) => {
+//   let pageInfo = {
+//     user_id: localStorage.user_id,
+//     page_name: page,
+//   }
+//   trafficFunctions('page', 'http://localhost:3001/pages', pageInfo)
+// }
