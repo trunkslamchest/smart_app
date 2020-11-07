5bc6f80790796168b3601ba4c58f958d5c679b60
trunkslamchest <trunk9slamchest@gmail.com>
2020-11-07 00:20:02 -0500

    added previous and next buttons to leaderboard pages

---
+++ src/assets/glyphs/leaderboardGlyphIndex.js
@@ -0,0 +1,19 @@
+import leaderboardNext from './leaderboard/next_leaderboard_button.png'
+import leaderboardNextHover from './leaderboard/next_hover_leaderboard_button.png'
+import leaderboardNextDisabled from './leaderboard/next_disabled_leaderboard_button.png'
+
+import leaderboardPrev from './leaderboard/prev_leaderboard_button.png'
+import leaderboardPrevHover from './leaderboard/prev_hover_leaderboard_button.png'
+import leaderboardPrevDisabled from './leaderboard/prev_disabled_leaderboard_button.png'
+
+
+const leaderboardGlyphIndex = {
+  'leaderboardNext': leaderboardNext,
+  'leaderboardNextHover': leaderboardNextHover,
+  'leaderboardNextDisabled': leaderboardNextDisabled,
+  'leaderboardPrev': leaderboardPrev,
+  'leaderboardPrevHover': leaderboardPrevHover,
+  'leaderboardPrevDisabled': leaderboardPrevDisabled
+}
+
+export default leaderboardGlyphIndex
--- src/leaderboards/leaderBoardsComponents/leaderBoardsButtonsRow/leaderBoardsButtonsRow.js
+++ src/leaderboards/leaderBoardsComponents/leaderBoardsButtonsRow/leaderBoardsButtonsRow.js
@@ -1,31 +1,88 @@
 import React from 'react'
 
+import leaderboardGlyphIndex from '../../../assets/glyphs/leaderboardGlyphIndex'
+
 import './leaderBoardsButtonsRow.css'
 
-const LeaderBoardsButtonsRow = (props) => {
+class LeaderBoardsButtonsRow extends React.Component {
 
-  const onClickPageFunctions = (event) => { if(props.currentPage >= 0 && props.currentPage <= props.maxPages) props.onChangePage(event.target.value) }
+  state={ prevHover: false, nextHover: false }
 
-  return(
-    <div className="leader_boards_buttons_row_container">
-      <button
-        className="leader_boards_buttons_row_button"
-        disabled={ props.currentPage === 0 }
-        onClick={ onClickPageFunctions }
-        value={ -1 }
-      >
-        Previous Page
-      </button>
-      <button
-        className="leader_boards_buttons_row_button"
-        disabled={ props.currentPage === props.maxPages - 1 }
-        onClick={ onClickPageFunctions }
-        value={ 1 }
-      >
-        Next Page
-      </button>
-    </div>
-  )
+  componentDidUpdate() {
+    if(this.props.currentPage === 0 && this.state.prevHover) this.setState({ prevHover: false })
+    if(this.props.currentPage === this.props.maxPages - 1 && this.state.nextHover) this.setState({ nextHover: false })
+  }
+
+  onPrevHover = () => { this.setState({ prevHover: true }) }
+  offPrevHover = () => { this.setState({ prevHover: false }) }
+
+  onNextHover = () => { this.setState({ nextHover: true }) }
+  offNextHover = () => { this.setState({ nextHover: false }) }
+
+  onClickPageFunctions = (event) => {
+    let val = event.target.value || event.target.attributes.value.value
+    if(this.props.currentPage >= 0 && this.props.currentPage <= this.props.maxPages) this.props.onChangePage(val) 
+  }
+
+  render() {
+
+    console.log(this.state)
+
+    let prevButton, nextButton
+
+    if(this.props.currentPage === 0) {
+      prevButton = leaderboardGlyphIndex.leaderboardPrevDisabled
+    } else {
+      if(this.state.prevHover) prevButton = leaderboardGlyphIndex.leaderboardPrevHover
+      else prevButton = leaderboardGlyphIndex.leaderboardPrev
+    }
+
+    if(this.props.currentPage === this.props.maxPages - 1) {
+      nextButton = leaderboardGlyphIndex.leaderboardNextDisabled
+    } else {
+      if(this.state.nextHover) nextButton = leaderboardGlyphIndex.leaderboardNextHover
+      else nextButton = leaderboardGlyphIndex.leaderboardNext
+    }
+
+    return(
+      <div className="leader_boards_buttons_row_container">
+        <button
+          className="leader_boards_buttons_row_button"
+          disabled={ this.props.currentPage === 0 }
+          onClick={ this.onClickPageFunctions }
+          onMouseEnter={ this.onPrevHover }
+          onMouseLeave={ this.offPrevHover }
+          value={ -1 }
+        >
+          <img
+            id='leaderboard_prev_button'
+            alt='PreviousPageButton'
+            name='leaderboardPrevButton'
+            onClick={ this.onClickPageFunctions }
+            src={ prevButton }
+            value={ -1 }
+          />
+        </button>
+        <button
+          className="leader_boards_buttons_row_button"
+          disabled={ this.props.currentPage === this.props.maxPages - 1 }
+          onClick={ this.onClickPageFunctions }
+          onMouseEnter={ this.onNextHover }
+          onMouseLeave={ this.offNextHover }
+          value={ 1 }
+        >
+          <img
+            id='leaderboard_next_button'
+            alt='NextPageButton'
+            name='leaderboardNextButton'
+            onClick={ this.onClickPageFunctions }
+            src={ nextButton }
+            value={ 1 }
+          />
+        </button>
+      </div>
+    )
+  }
 }
 
 export default LeaderBoardsButtonsRow
