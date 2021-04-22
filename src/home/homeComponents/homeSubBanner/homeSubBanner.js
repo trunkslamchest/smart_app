63c6bfc7668d7a14e397396d0fdf881d3e8ce202
trunkslamchest <trunk9slamchest@gmail.com>
2021-04-22 13:14:50 -0400

    added background to homeSubBanner

--- src/home/homeComponents/homeSubBanner/homeSubBanner.js
+++ src/home/homeComponents/homeSubBanner/homeSubBanner.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import whiteHomeSubBannerLogo from '../../../assets/logos/indexLogos/white_home_sub_banner_logo.svg'
+import whiteHomeSubBannerLogo from '../../../assets/home_sub_banner/white_home_sub_banner_logo.svg'
 
 import './homeSubBanner.css'
 
@@ -8,21 +8,23 @@ const HomeSubBanner = (props) => {
 
   return(
     <div className='home_logged_out_sub_banner'>
-      <div className='home_logged_out_text_container'>
-        <h2>
-          Answer Questions. Unlock Achievements. Improve Your Rank. Climb The Leaderboards.
-        </h2>
-        <h3>
-          Get Smart.
-        </h3>
-      </div>
-      <div className='home_logged_out_img_container'>
-        <img
-          alt='PP'
-          id='home_sub_banner_logo'
-          name='homeSubBannerLogo'
-          src={ whiteHomeSubBannerLogo }
-        />
+      <div className='home_logged_out_sub_banner_wrapper'>
+        <div className='home_logged_out_text_container'>
+          <h2>
+            Answer Questions. Unlock Achievements. Improve Your Rank. Climb The Leaderboards.
+          </h2>
+          <h3>
+            Get Smart.
+          </h3>
+        </div>
+        <div className='home_logged_out_img_container'>
+          <img
+            alt='PP'
+            id='home_sub_banner_logo'
+            name='homeSubBannerLogo'
+            src={ whiteHomeSubBannerLogo }
+          />
+        </div>
       </div>
     </div>
   )
