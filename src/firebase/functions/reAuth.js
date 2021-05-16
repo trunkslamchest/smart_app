5db7f79e9a6dd5ef8eda40a924aa4b3e6fdf48f7
trunkslamchest <trunk9slamchest@gmail.com>
2021-05-16 19:30:00 -0400

    switch back to local development environment to fix CSS scaling issues

--- src/firebase/functions/reAuth.js
+++ src/firebase/functions/reAuth.js
@@ -1,5 +1,5 @@
 import init from '../../firebase/init';
-// init.auth().useEmulator("http://localhost:8004")
+init.auth().useEmulator("http://localhost:8004")
 
 async function reAuth(email, password){
 
