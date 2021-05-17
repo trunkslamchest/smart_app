a578da4cf579ac0f4f30368e5f51d719fe226308
trunkslamchest <trunk9slamchest@gmail.com>
2021-05-17 14:22:16 -0400

    minor cleanup to makeFooterLinkButtons function

--- src/UI/footer/footerFunctions/makeFooterLinkButtons.js
+++ src/UI/footer/footerFunctions/makeFooterLinkButtons.js
@@ -1,8 +1,4 @@
 const makeFooterLinkButtons = (onPushLink, routes) => {
-    // { name: 'footerDisclaimer', id: 'footer_disclaimer_button', route: routes.disclaimer, text: 'Disclaimer' },
-    // { name: 'footerLicense', id: 'footer_license_button', route: routes.license, text: 'License' },
-    // { name: 'footerPrivacyPolicy', id: 'footer_privacy_button', route: routes.privacy, text: 'Privacy Policy' },
-    // { name: 'footerTOS', id: 'footer_tos_button', route: routes.tos, text: 'Terms of Service' }
   return [
     {
       buttonType: 'NavLink',
@@ -11,7 +7,6 @@ const makeFooterLinkButtons = (onPushLink, routes) => {
       onClickFunction: onPushLink,
       params: JSON.stringify({ route: routes.disclaimer }),
       route: routes.disclaimer,
-      // target: '_blank',
       text: 'Disclaimer'
     },
     {
@@ -21,7 +16,6 @@ const makeFooterLinkButtons = (onPushLink, routes) => {
       onClickFunction: onPushLink,
       params: JSON.stringify({ route: routes.license }),
       route: routes.license,
-      // target: '_blank',
       text: 'License'
     },
     {
@@ -31,7 +25,6 @@ const makeFooterLinkButtons = (onPushLink, routes) => {
       onClickFunction: onPushLink,
       params: JSON.stringify({ route: routes.privacy }),
       route: routes.privacy,
-      // target: '_blank',
       text: 'Privacy Policy'
     },
     {
@@ -41,7 +34,6 @@ const makeFooterLinkButtons = (onPushLink, routes) => {
       onClickFunction: onPushLink,
       params: JSON.stringify({ route: routes.tos }),
       route: routes.tos,
-      // target: '_blank',
       text: 'Terms of Service'
     },
   ]
