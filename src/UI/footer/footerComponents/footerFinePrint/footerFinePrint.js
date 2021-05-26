e5bc6de4b88da13c5e2ad5e98292538d87e74e93
trunkslamchest <trunk9slamchest@gmail.com>
2021-05-26 17:36:02 -0400

    built and deployed version 0.96

--- src/UI/footer/footerComponents/footerFinePrint/footerFinePrint.js
+++ src/UI/footer/footerComponents/footerFinePrint/footerFinePrint.js
@@ -15,7 +15,7 @@ const FooterFinePrint = () => {
        All Rights Reserved
       </p>
       <p className='footer_fine_print_text'>
-        0.96.05.26.21
+        0.97.05.26.21
       </p>
     </div>
   )
