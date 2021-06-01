e1e7c5106801642321f43c2be61acf11345c2e4a
trunkslamchest <trunk9slamchest@gmail.com>
2021-06-01 12:43:43 -0400

    removed remaining unused art assets

--- src/assets/glyphs/glyphIndex.js
+++ src/assets/glyphs/glyphIndex.js
@@ -1,62 +1,31 @@
 import formWhiteCheckMark from './white_checkmark.svg'
 import formGreyCheckMark from './grey_checkmark.svg'
-import formBlackCheckMark from './black_checkmark.svg'
-import formOrangeCheckMark from './orange_checkmark.svg'
-
 import formWhiteX from './white_x.svg'
 import formGreyX from './grey_x.svg'
-
-import formBlackX from './black_x.svg'
-import formOrangeX from './orange_x.svg'
-
 import formWhiteReset from './white_reset.svg'
 import formGreyReset from './grey_reset.svg'
-import formBlackReset from './black_reset.svg'
-import formOrangeReset from './orange_reset.svg'
-
 import whiteEdit from './white_edit.svg'
-import blackEdit from './black_edit.svg'
 import greyEdit from './grey_edit.svg'
-import orangeEdit from './orange_edit.svg'
-
 import whiteView from './white_view.svg'
-import blackView from './black_view.svg'
 import greyView from './grey_view.svg'
-import orangeView from './orange_view.svg'
-
 import whiteDelete from './white_delete.svg'
-import blackDelete from './black_delete.svg'
 import greyDelete from './grey_delete.svg'
-import orangeDelete from './orange_delete.svg'
-
 import greenCheckMark from './green_checkmark.svg'
 import redX from './red_x.svg'
 
 const glyphIndex = {
   'formWhiteCheckMark': formWhiteCheckMark,
   'formGreyCheckMark': formGreyCheckMark,
-  'formBlackCheckMark': formBlackCheckMark,
-  'formOrangeCheckMark': formOrangeCheckMark,
   'formWhiteX': formWhiteX,
   'formGreyX': formGreyX,
-  'formBlackX': formBlackX,
-  'formOrangeX': formOrangeX,
   'formWhiteReset': formWhiteReset,
   'formGreyReset': formGreyReset,
-  'formBlackReset': formBlackReset,
-  'formOrangeReset': formOrangeReset,
   'whiteEdit': whiteEdit,
-  'blackEdit': blackEdit,
   'greyEdit': greyEdit,
-  'orangeEdit': orangeEdit,
   'whiteView': whiteView,
-  'blackView': blackView,
   'greyView': greyView,
-  'orangeView': orangeView,
   'whiteDelete': whiteDelete,
-  'blackDelete': blackDelete,
   'greyDelete': greyDelete,
-  'orangeDelete': orangeDelete,
   'greenCheckMark': greenCheckMark,
   'redX': redX
 }
--- src/assets/nav_bar_icons/dashboardNavBarIconIndex.js
+++ src/assets/nav_bar_icons/dashboardNavBarIconIndex.js
@@ -1,38 +1,24 @@
 import profileNavBarIconWhite from './white_profile_nav_bar_button.svg'
-import profileNavBarIconBlack from './black_profile_nav_bar_button.svg'
 import profileNavBarIconGrey from './grey_profile_nav_bar_button.svg'
-
 import statsNavBarIconWhite from './white_stats_nav_bar_button.svg'
-import statsNavBarIconBlack from './black_stats_nav_bar_button.svg'
 import statsNavBarIconGrey from './grey_stats_nav_bar_button.svg'
-
 import achievementsNavBarIconWhite from './white_achievements_nav_bar_button.svg'
-import achievementsNavBarIconBlack from './black_achievements_nav_bar_button.svg'
 import achievementsNavBarIconGrey from './grey_achievements_nav_bar_button.svg'
-
 import settingsNavBarIconWhite from './white_settings_nav_bar_button.svg'
-import settingsNavBarIconBlack from './black_settings_nav_bar_button.svg'
 import settingsNavBarIconGrey from './grey_settings_nav_bar_button.svg'
-
 import helpNavBarIconWhite from './white_help_nav_bar_button.svg'
-import helpNavBarIconBlack from './black_help_nav_bar_button.svg'
 import helpNavBarIconGrey from './grey_help_nav_bar_button.svg'
 
 const dashboardNavBarIconIndex = {
   'profileNavBarIconWhite': profileNavBarIconWhite,
-  'profileNavBarIconBlack': profileNavBarIconBlack,
   'profileNavBarIconGrey': profileNavBarIconGrey,
   'statsNavBarIconWhite': statsNavBarIconWhite,
-  'statsNavBarIconBlack': statsNavBarIconBlack,
   'statsNavBarIconGrey': statsNavBarIconGrey,
   'achievementsNavBarIconWhite': achievementsNavBarIconWhite,
-  'achievementsNavBarIconBlack': achievementsNavBarIconBlack,
   'achievementsNavBarIconGrey': achievementsNavBarIconGrey,
   'settingsNavBarIconWhite': settingsNavBarIconWhite,
-  'settingsNavBarIconBlack': settingsNavBarIconBlack,
   'settingsNavBarIconGrey': settingsNavBarIconGrey,
   'helpNavBarIconWhite': helpNavBarIconWhite,
-  'helpNavBarIconBlack': helpNavBarIconBlack,
   'helpNavBarIconGrey': helpNavBarIconGrey
 }
 
